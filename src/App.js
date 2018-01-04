25d612dc3b864d699a25db2385021c7bffbb62de
Gerard Simpson <gerardsimpson@Gerards-Mac.local>
2018-01-04 13:49:54 +1000

    refactoring into methods

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       threads: [],
       threadsShown: 0
     };
-    this.showMore = this.showMore.bind(this);
+    this.getStories = this.getStories.bind(this);
   }
 
   render() {
@@ -37,68 +37,55 @@ class App extends Component {
         <section className='bg-brown pa2'>
             {this.state.threads.map((data, index) =>
               <div className='fl pv2 w-100'>
-                <span className='color-secondary pr1'>{this.state.threadsShown + index - this.batchNumber}.</span>
+                <span className='color-secondary pr1'>{this.postIndex(index)}.</span>
                 <span className='color-secondary pr1'>&#8593;</span>
                 <span>{data.title}</span>
                 <span className='color-secondary pl4 fl w-100 f6'>{data.score} points by {data.by} {this.convertToTime(data.time)} | hide | {data.descendants} comments</span>
               </div>
             )}
-          <a onClick={this.showMore}>More</a>
+          <a onClick={this.getStories}>More</a>
         </section>
       </div>
     );
   }
 
+  // no index in returned JSON, this sets index
+  postIndex(index) {
+    return this.state.threadsShown - this.batchNumber + index + 1;
+  }
+
+  // time comes in 1515025917 format, needs to be converted
   convertToTime(time) {
     let hoursAgo = (new Date() - new Date(time * 1000))/1000/60/60;
     return hoursAgo < 1 ? Math.floor(hoursAgo * 60) + ' minutes ago' :  Math.floor(hoursAgo) + ' hours ago';
+  }
+
+  componentDidMount() {
+    this.getStoryIds();
+    this.getStories();
   }
 
   componentWillUnmount() {
     this.topStories.unsubscribe();
   }
 
-  componentDidMount() {
-
-    this.topStories$ = Rx.Observable
+  getStoryIds() {
+    this.topStoryIds$ = Rx.Observable
       .ajax(`${this.baseUrl}/topstories.json`)
       .publishLast()
       .refCount()
       .pluck('response')
-
-    this.topStoriesIds$ = this.topStories$.switchMap(data => data);
-
-    this.topStoriesIds3$ = this.topStoriesIds$
-      .take(this.batchNumber)
-      .concatMap(id => Rx.Observable.ajax(`${this.baseUrl}/item/${id}.json`))
-      .pluck('response')
-
-    //subscribe
-    this.topStoriesIds3$.subscribe(
-      (data) => {
-        this.idArray.push(data)
-        console.log('Data: ', data);
-      },
-      (error) => console.log("ERROR:", error),
-      () => {
-        this.setState({
-         threads: this.idArray,
-         threadsShown: this.state.threadsShown + this.batchNumber
-        })
-        this.idArray = [];
-      }
-    );
+      .switchMap(data => data);
   }
-
-  showMore(){
-    this.topStoriesIds$
+  getStories() {
+    this.topStoryIds$
       .skip(this.state.threadsShown)
       .take(this.batchNumber)
       .concatMap(id => Rx.Observable.ajax(`${this.baseUrl}/item/${id}.json`))
       .pluck('response')
       .subscribe(
         (data) => this.idArray.push(data),
-        (error) => console.log("ERROR:", error),
+        (error) => console.log('error: ', error),
         () => {
           this.setState({
             threads: this.idArray,
